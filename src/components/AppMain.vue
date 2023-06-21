50b49ecf4c5e8e4cbb3eb3190a52e892979c9f2b
Alessio-DiNardo <Alessiodinardo9@gmail.comm>
2023-06-21 15:46:56 +0200

    add search bar

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,11 +1,9 @@
 <template>
     <div>
-        <h1>
-            AppMain
-        </h1>
-        <h2>
-            {{ store.searchText }}
-        </h2>
+        <form class="d-flex" role="search">
+            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+            <button class="btn btn-outline-success" type="submit">Search</button>
+        </form>
         <MovieCard/>
     </div>
 </template>
