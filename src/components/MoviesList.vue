dc1854578931f2733fc9f75d75b8d6a376a71cb5
Alessio-DiNardo <Alessiodinardo9@gmail.comm>
2023-06-25 14:47:43 +0200

    try to add tv series

--- src/components/MoviesList.vue
+++ src/components/MoviesList.vue
@@ -1,45 +1,22 @@
 <template>
-    <div>
-        <article v-for="movie in movies">
-            <h2>
-                {{ movie.title }}--{{ movie.original_title }}
-            </h2>
-            <img v-if="isLanguageAvalable(movie.original_language)"  
-            :src="getImagePath(movie.original_language)" alt="Language flag" class="language-flag">
-            <span v-else>
-                {{ movie.original_language }}
-            </span>
-            <p>
-                {{ movie.vote_average }}
-            </p>
-        </article>
+    <div class="card-wrapper">
+            <SingleCard  v-for="movie in movies" :movie="movie"/>
     </div>
 </template>
 <script>
-
+import SingleCard from './SingleCard.vue';
 export default {
     
     name: 'MovieList',
+    components: {
+        SingleCard
+    },
     props: {
         movies: Array
-
-    },
-    methods: {
-        isLanguageAvalable(language){
-            if (this.availableFlags.includes(language + '.png')){
-                return true
-            }
-
-            return false;
-        },
-
-        getImagePath(imgSource){
-            return new URL(`../assets/img/flags${imgSource}.png`, import.meta.url).href;
-        }
     },
     data() {
         return {
-            availableFlags : ['en.png','fr.png','it.png','ja.png','ru.png']
+            
         }
     },
 }
---
+++ src/components/SingleCard.vue
@@ -0,0 +1,45 @@
+<template>
+    
+    <div class="cards-title">
+        <h2>
+            {{ movie.title }}-- <em>{{ movie.original_title }}</em>
+        </h2>
+    </div>
+    <div class="cardbody">
+        <img  v-if="isLanguageAvailable(movie.original_language)" :src="getImagePath(movie.original_language)" alt="Language flag" class="language-flag">
+        <span v-else>
+            {{ movie.original_language }}
+        </span>
+        <p>
+            {{ movie.vote_average }}
+        </p>
+    </div>
+</template>
+<script>
+
+export default {
+    name: 'SingleCard', 
+    data() {
+        return {
+            availableFlags : ['en.png','fr.png','it.png','ja.png','ru.png']
+        }
+    },
+    methods:{
+        isLanguageAvalable(language){
+            if (this.availableFlags.includes(language + '.png')){
+                return true
+            }
+
+            return false;
+        },
+
+        getImagePath(imgSource){
+            return new URL(`../assets/img/flags${imgSource}.png`, import.meta.url).href;
+        }
+    },
+    }
+}
+</script>
+<style lang="scss" scoped>
+    
+</style>
